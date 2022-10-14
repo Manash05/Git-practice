ed1b8a505871d940a75727f39b4e73d3c99a2cab
Manash Pratim Kalita <manashpk05@gmail.com>
2022-10-14 07:58:50 +0530

    adding sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int n, m, s;
+    printf("Enter the first number: ");
+    scanf("%d", &n);
+    printf("Enter the second number: "):
+    scanf("%d", &m);
+    s = n + m;
+    printf("The sum of %d and %d is %d.\n", n, m, s);
+    return 0;
+}
