28b602e8eb0defa53fe7239602d4c3048c5062f8
Manash Pratim Kalita <manashpk05@gmail.com>
2022-10-14 08:10:52 +0530

    correction

--- sum.c
+++ sum.c
@@ -4,7 +4,7 @@ int main(){
     int n, m, s;
     printf("Enter the first number: ");
     scanf("%d", &n);
-    printf("Enter the second number: "):
+    printf("Enter the second number: ");
     scanf("%d", &m);
     s = n + m;
     printf("The sum of %d and %d is %d.\n", n, m, s);
